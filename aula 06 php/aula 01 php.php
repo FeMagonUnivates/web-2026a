cda950596eaf73e03f3d4dc9473e6be2f8908ee2
FERNANDO MAGON WEIZENMANN <fernando.weizenmann@acad.univates>
2026-04-07 20:01:44 -0300

    Começar php

---
+++ aula 06 php/aula 01 php.php
@@ -0,0 +1,13 @@
+<?php
+    $nome = "Maria";
+    $idade = 20;
+    echo "nome: " . $nome . "<br>";
+    echo "Idade: " . $idade . " anos <br>";
+    echo "Idade: $idade anos";
+
+    if ($idade >= 18) {
+        echo "Você é meior de idade";
+    } else {
+        echo "Você é menor de idade";
+    }
+?>
